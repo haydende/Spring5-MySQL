1735df41ed0cf4ca9fdbfd0250341178edd73c37
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-08-18 13:34:04 +0100

    Initial project added

---
+++ src/main/java/haydende/spring5mysqldemo/Spring5MysqlDemoApplication.java
@@ -0,0 +1,13 @@
+package haydende.spring5mysqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5MysqlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5MysqlDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/haydende/spring5mysqldemo/Spring5MysqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package haydende.spring5mysqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5MysqlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
