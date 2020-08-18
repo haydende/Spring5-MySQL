16e08106d90b043f5591f645af36bb4f5f7561a4
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-08-18 16:24:24 +0100

    Corrected order of Type parameters in the Repository interfaces.

--- src/main/java/haydende/spring5mysqldemo/repository/SubjectRepository.java
+++ src/main/java/haydende/spring5mysqldemo/repository/SubjectRepository.java
@@ -1,6 +1,7 @@
 package haydende.spring5mysqldemo.repository;
 
+import haydende.spring5mysqldemo.domain.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubjectRepository extends JpaRepository<String, SubjectRepository> {
+public interface SubjectRepository extends JpaRepository<Subject, String> {
 }
