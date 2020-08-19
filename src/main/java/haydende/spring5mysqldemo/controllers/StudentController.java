9616f8382e90ab5c826ea2b727955f6176fe3783
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-08-19 14:12:23 +0100

    Added StudentController.
    Closes #1

---
+++ src/main/java/haydende/spring5mysqldemo/controllers/StudentController.java
@@ -0,0 +1,27 @@
+package haydende.spring5mysqldemo.controllers;
+
+import haydende.spring5mysqldemo.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/students/")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @RequestMapping("")
+    public String findAll(Model model) {
+        log.error("Invoking StudentController.findAll()");
+        model.addAttribute("students", studentService.findAll());
+        return "/students/students";
+    }
+
+}
---
+++ src/test/java/haydende/spring5mysqldemo/controller/StudentControllerTest.java
@@ -0,0 +1,53 @@
+package haydende.spring5mysqldemo.controller;
+
+import haydende.spring5mysqldemo.controllers.StudentController;
+import haydende.spring5mysqldemo.domain.Student;
+import haydende.spring5mysqldemo.service.StudentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class StudentControllerTest {
+
+    public static final String URL = "/students/";
+
+    @Mock
+    StudentService studentService;
+
+    @InjectMocks
+    StudentController studentController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(studentController).build();
+    }
+
+    @Test
+    void findAll() throws Exception {
+
+        BDDMockito
+            .given(studentService.findAll())
+            .willReturn(
+                Arrays.asList(
+                    Student.builder().build(),
+                    Student.builder().build()));
+
+        mockMvc.perform(get(URL))
+            .andExpect(status().isOk())
+            .andExpect(view().name("/students/students"))
+            .andExpect(model().attributeExists("students"));
+    }
+}
