2dff928ea01dd1e1ccf8ee8ffadea88a66d756f0
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-08-19 14:08:21 +0100

    Added IndexController.
    Closes #4

---
+++ src/main/java/haydende/spring5mysqldemo/controllers/IndexController.java
@@ -0,0 +1,30 @@
+package haydende.spring5mysqldemo.controllers;
+
+import haydende.spring5mysqldemo.service.StudentService;
+import haydende.spring5mysqldemo.service.SubjectService;
+import haydende.spring5mysqldemo.service.TeacherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    private final TeacherService teacherService;
+    private final SubjectService subjectService;
+    private final StudentService studentService;
+
+    public IndexController(TeacherService teacherService, SubjectService subjectService, StudentService studentService) {
+        this.teacherService = teacherService;
+        this.subjectService = subjectService;
+        this.studentService = studentService;
+    }
+
+    @RequestMapping("/")
+    public String getAll(Model model) {
+        model.addAttribute("teachers", teacherService.findAll());
+        model.addAttribute("students", studentService.findAll());
+        model.addAttribute("subjects", subjectService.findAll());
+        return "index";
+    }
+}
---
+++ src/test/java/haydende/spring5mysqldemo/controller/IndexControllerTest.java
@@ -0,0 +1,74 @@
+package haydende.spring5mysqldemo.controller;
+
+import haydende.spring5mysqldemo.controllers.IndexController;
+import haydende.spring5mysqldemo.domain.Student;
+import haydende.spring5mysqldemo.domain.Subject;
+import haydende.spring5mysqldemo.domain.Teacher;
+import haydende.spring5mysqldemo.service.StudentService;
+import haydende.spring5mysqldemo.service.SubjectService;
+import haydende.spring5mysqldemo.service.TeacherService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+// import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+// import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+class IndexControllerTest {
+
+    @Mock
+    TeacherService teacherService;
+
+    @Mock
+    SubjectService subjectService;
+
+    @Mock
+    StudentService studentService;
+
+    @InjectMocks
+    IndexController indexController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+    }
+
+    @Test
+    void getAll() throws Exception {
+        BDDMockito
+            .given(teacherService.findAll())
+            .willReturn(
+                Arrays.asList(Teacher.builder().build()));
+
+        BDDMockito
+            .given(studentService.findAll())
+            .willReturn(
+                Arrays.asList(
+                    Student.builder().build()));
+
+        BDDMockito
+            .given(subjectService.findAll())
+            .willReturn(
+                Arrays.asList(
+                    Subject.builder().build()));
+
+        mockMvc
+            .perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("index"))
+            .andExpect(model().attributeExists("teachers", "students", "subjects"));
+    }
+}
